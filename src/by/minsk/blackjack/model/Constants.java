ac392178660ef9ca31ee5d03544bb506d9331c5b
AliakseiBoltak <AliakseiBoltak@users.noreply.github.com>
2016-10-17 12:59:05 +0300

    Update Constants.java

--- src/by/minsk/blackjack/model/Constants.java
+++ src/by/minsk/blackjack/model/Constants.java
@@ -4,7 +4,7 @@ public final class Constants {
 	public static final int PLAYER_BANK = 1000;
 	public static final int BOT_BANK = 1000;
 	public static final String BOT_NAME = "Bot";
-	public static final int DILER_BANK = 100000000;
+	public static final int DILER_BANK = 7777777777;
 	public static final String DILER_NAME = "Diler";
 
 }

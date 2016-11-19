63ccddba9a85fd66b7a3c6368783aa9ceffc66c6
Aliaksei Boltak <aaboltak@gmail.com>
2016-11-19 22:53:44 +0300

    author

--- src/by/minsk/blackjack/model/Constants.java
+++ src/by/minsk/blackjack/model/Constants.java
@@ -1,10 +1,13 @@
 package by.minsk.blackjack.model;
-
+/*
+created by Aliaksei Boltak
+*/
 public final class Constants {
 	public static final int PLAYER_BANK = 1000;
 	public static final int BOT_BANK = 1000;
 	public static final String BOT_NAME = "SuperMegaBot";
 	public static final int DILER_BANK = 777777;
 	public static final String DILER_NAME = "SuperMegaDrugDiler";
-//////////////////////////////////////
+
+	
 }
